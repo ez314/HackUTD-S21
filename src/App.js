6eb5972101a2edaa00d995582ebabb29d62b7aa7
Sunny Guan <sunny.peng.guan@gmail.com>
2021-02-27 12:44:15 -0800

    added frontend

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center p-4">
+      <h1 className="text-3xl">Rectangle</h1>
+    </div>
+  );
+}
+
+export default App;
