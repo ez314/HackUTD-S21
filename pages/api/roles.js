57b86d3cd68c2db0359c2f7475744e6bb8e3c7a1
Sunny Guan <sunny.peng.guan@gmail.com>
2021-02-27 17:43:44 -0800

    updated calls

---
+++ pages/api/roles.js
@@ -0,0 +1,28 @@
+import { db } from "../../lib/firebase";
+
+async function check(courses) {
+  let result = [];
+  console.log(courses);
+  for (let course of courses) {
+      console.log(course);
+    await result.push(await db
+      .collection("faq").doc(course).get().then(res => res.data()));
+  }
+  console.log(result);
+  return result;
+}
+
+export default async function handler(req, res) {
+  const id = req.query.id;
+  await db
+    .collection("user")
+    .where("id", "==", id)
+    .get()
+    .then(async (querysnapshot) => {
+      const courses = querysnapshot.docs[0].data()["courses"];
+      res.json(await check(courses));
+    })
+    .catch((error) => {
+      res.json({});
+    });
+}
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,25 @@
-export default function Dashboard({user}) {
+import { data } from "autoprefixer";
+import { useEffect, useState } from "react";
+
+export default function Dashboard({ user }) {
+
+  const [courses, setCourses] = useState(null);
+
+  if (user.id === undefined) {
+    return <p>Not logged in!</p>;
+  }
+
+  if (user.id !== undefined && courses === null) {
     console.log(user);
-    if (!user) {
-        return (
-            <p>Not logged in!</p>
-        )
-    }
+    fetch(`http://localhost:3000/api/roles?id=${user.id}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data)
+          setCourses(data)
+      })
+  }
+
   return (
     <div className="mx-auto text-center mt-4">
       <img
@@ -14,6 +29,14 @@ export default function Dashboard({user}) {
       <h1 className="text-3xl mt-4">
         Hello, {user.username}#{user.discriminator}!
       </h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 m-4">
+        {courses && courses.map(course => (
+          <div id={course.faqID} className="bg-blue-200 p-4 text-center rounded-lg shadow-3 text-xl">
+            {course.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
+
 }
