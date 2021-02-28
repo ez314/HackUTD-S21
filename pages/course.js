ff451adadb54198a1a234c558e235a53440897d2
Sunny Guan <sunny.peng.guan@gmail.com>
2021-02-28 01:36:26 -0800

    updated msg

--- pages/course.js
+++ pages/course.js
@@ -14,16 +14,21 @@ export default function Course({ id, name }) {
       });
   }
 
+  console.log(data);
   return (
     <div className="bg-gray-800 px-16 pb-16">
-      <h1
-        className="flex-1 font-bold text-3xl text-center text-blue-50 place-self-center tracking-normal">FAQs
-        for {name}</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-        {data !== [] &&
-        data !== null &&
-        data.map((faq, index) => <FAQ faq={faq} key={index}/>)}
-      </div>
+      <h1 className="flex-1 font-bold text-3xl text-center text-blue-50 place-self-center tracking-normal">
+        FAQs for {name}
+      </h1>
+      {data !== null && data !== undefined && data.length !== 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+          {data.map((faq, index) => <FAQ faq={faq} key={index} />)}
+        </div>
+      ) : (
+        <p className="text-center text-blue-50 w-full mt-8 font-light">
+          No FAQs found for this course.
+        </p>
+      )}
     </div>
   );
 }
